bb477cdb68ccac890879bc09b9c58fc8addb894a
barhrhouj hamza <barhrhoujhamza@gmail.com>
2024-11-07 04:29:36 +0100

    adding react query for past orders

--- src/routes/__root.jsx
+++ src/routes/__root.jsx
@@ -4,6 +4,7 @@ import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 import { CartContext } from "../contexts";
 import Header from "../Header";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export const Route = createRootRoute({
   component: () => {
@@ -18,6 +19,7 @@ export const Route = createRootRoute({
           </div>
         </CartContext.Provider>
         <TanStackRouterDevtools />
+        <ReactQueryDevtools></ReactQueryDevtools>
       </>
     );
   },
