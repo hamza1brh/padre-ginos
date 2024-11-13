d6e9717f6de1e2528e71a30e19668274f211d959
barhrhouj hamza <barhrhoujhamza@gmail.com>
2024-11-13 22:29:46 +0100

    uncontrolled form

--- src/routes/index.lazy.jsx
+++ src/routes/index.lazy.jsx
@@ -16,7 +16,10 @@ function Index() {
           <Link to="/order">Order</Link>
         </li>
         <li>
-          <Link to="past"> Past Orders</Link>
+          <Link to="/past"> Past Orders</Link>
+        </li>
+        <li>
+          <Link to="contact"> Contact us </Link>
         </li>
       </ul>
     </div>
